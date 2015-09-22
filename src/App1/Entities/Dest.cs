55f4d9863944b863620e2e034ceb620e46294369
unknown <nelsong@NELSONG.LagashSystems.local>
2015-09-21 21:58:55 -0300

    Added sample project

---
+++ src/App1/Entities/Dest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App1
+{
+    public class Dest
+    {
+        public string Title { get; set; }
+
+        public string TitleThatWillBreakMapping { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ src/App1/Entities/Profile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace App1
+{
+    class MappingProfile : Profile
+    {
+        public override string ProfileName
+        {
+            get { return this.GetType().Name; }
+        }
+
+        protected override void Configure()
+        {
+            this.CreateMap<Source, Dest>()
+                 .ForMember(x => x.Title, x => x.MapFrom(y => y.Title))
+                 .ForMember(x => x.Description, x => x.MapFrom(y => y.Description));
+        }
+    }
+}
